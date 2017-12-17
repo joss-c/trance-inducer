943c1b6af6eb59a4d516ddd967ff2d6d19c44cd6
joss-c <joss.classey@gmail.com>
2017-12-17 10:15:01 +0400

    Removed useless code
    Removed useless JSON.stringify & parse and array of 1s

--- tranceinducer.js
+++ tranceinducer.js
@@ -4,7 +4,6 @@ var button = document.getElementById("update");
 var interval = 100;
 var arrayIndex = 0;
 
-
 // LFO ARRAY CREATOR
 function lfo(min, max, speed, waveForm, waveLength) { // function generates array representing points on either a triangle or sawtooth wave, to 3 decimal places. min = sarting value (includes decimal). max = peak value (includes decimal). speed = distance between points (includes decimal). waveForm = triangle: 1, sawtooth: 2. length = 1 - 1000.
 		var wavePoints = [];
@@ -41,7 +40,6 @@ var lfo_2_saw_reversed = lfo_2_saw.slice(0);
 lfo_2_saw_reversed = lfo_2_saw_reversed.reverse();
 
 // LFO set for Square Sizes
-var lfo_3_1 = lfo(0,1,1,1,10);
 var lfo_3_3 = lfo(0,3,1,1,13);
 var lfo_3_5 = lfo(0,5,1,1,12);
 var lfo_3_10 = lfo(0,10,1,1,10);
@@ -49,13 +47,10 @@ var lfo_3_20 = lfo(0,20,1,1,8);
 var lfo_3_35 = lfo(0,35,1,1,7);
 var lfo_3_60 = lfo(0,60,1,1,6);
 
-
-
 setInterval(function() {
 	previewHandler();
 	arrayIndex++;
 }, interval)
-
 
 function previewHandler() {
 	var numberOfSquares = document.getElementById("numberOfSquares")[document.getElementById("numberOfSquares").selectedIndex].value
@@ -84,23 +79,22 @@ function rndNum(x,y) {
 	}
 }
 
-
 function drawSquare(canvas, context) {
 	var gradient = document.getElementById("gradient")[document.getElementById("gradient").selectedIndex].value;
 	var squareSize = document.getElementById("squareSize")[document.getElementById("squareSize").selectedIndex].value;
 	var colourScheme = document.getElementById("colourScheme")[document.getElementById("colourScheme").selectedIndex].value;
 	
 	// COLOUR SCHEMES
-	var colourScheme1 = JSON.stringify("rgb("+lfo_1[arrayIndex]+", "+lfo_1[arrayIndex+rndNum(50,100)]+", "+lfo_2[arrayIndex+rndNum(0,13)]+")");
-	var colourScheme2 = JSON.stringify("rgb("+lfo_2[arrayIndex]+", "+lfo_1[arrayIndex+rndNum(0,100)]+", "+lfo_1[arrayIndex]+")");
-	var colourScheme3 = JSON.stringify("rgb("+lfo_1[arrayIndex+rndNum(0,3)]+", "+lfo_1[arrayIndex+rndNum(0,5)]+", "+lfo_2_saw[arrayIndex]+")");
-	var colourScheme4 = JSON.stringify("rgb("+lfo_1[arrayIndex+rndNum(0,3)]+", "+lfo_2_saw_reversed[arrayIndex+rndNum(3,10)]+", "+lfo_2_saw[arrayIndex]+")");
+	var colourScheme1 = "rgb("+lfo_1[arrayIndex]+", "+lfo_1[arrayIndex+rndNum(50,100)]+", "+lfo_2[arrayIndex+rndNum(0,13)]+")";
+	var colourScheme2 = "rgb("+lfo_2[arrayIndex]+", "+lfo_1[arrayIndex+rndNum(0,100)]+", "+lfo_1[arrayIndex]+")";
+	var colourScheme3 = "rgb("+lfo_1[arrayIndex+rndNum(0,3)]+", "+lfo_1[arrayIndex+rndNum(0,5)]+", "+lfo_2_saw[arrayIndex]+")";
+	var colourScheme4 = "rgb("+lfo_1[arrayIndex+rndNum(0,3)]+", "+lfo_2_saw_reversed[arrayIndex+rndNum(3,10)]+", "+lfo_2_saw[arrayIndex]+")";
 
-	var w = (squareSize == "3" ? lfo_3_3 : squareSize == "5" ? lfo_3_5 : squareSize == "10" ? lfo_3_10 : squareSize == "20" ? lfo_3_20 : squareSize == "35" ? lfo_3_35 : squareSize == "60" ? lfo_3_60 : lfo_3_1)[arrayIndex];
+	var w = squareSize == "1" ? 1 : ((squareSize == "3" ? lfo_3_3 : squareSize == "5" ? lfo_3_5 : squareSize == "10" ? lfo_3_10 : squareSize == "20" ? lfo_3_20 : squareSize == "35" ? lfo_3_35 : lfo_3_60)[arrayIndex]);
 	var x = rndNum((gradient == "enabled" ? rndNum(0, canvas.width) : 0),canvas.width);
 	var y = Math.floor(Math.random() * canvas.height);
 	
-	context.fillStyle = JSON.parse((colourScheme == "1" ? colourScheme1 : colourScheme == "2" ? colourScheme2 : colourScheme == "3" ? colourScheme3 : colourScheme4));
+	context.fillStyle = (colourScheme == "1" ? colourScheme1 : colourScheme == "2" ? colourScheme2 : colourScheme == "3" ? colourScheme3 : colourScheme4);
 	context.fillRect(x, y, w, w);
 }
 
